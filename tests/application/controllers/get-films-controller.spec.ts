ff6e4ae908af581664cf95796cd5684a4202fee8
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-10-27 11:10:27 -0300

    feat: ensure GetFilms Controller returns serverError if getFilms throws

--- tests/application/controllers/get-films-controller.spec.ts
+++ tests/application/controllers/get-films-controller.spec.ts
@@ -2,10 +2,14 @@ import { GetFilms } from "@/domain/features";
 import { Film } from "@/domain/models";
 
 export class GetFilmsController {
-  constructor (private readonly getFilms: GetFilms) {}
+  constructor(private readonly getFilms: GetFilms) {}
 
-  async handle(): Promise<void> {
-    await this.getFilms.execute();
+  async handle(): Promise<any> {
+    try {
+      await this.getFilms.execute();
+    } catch (error) {
+      return serverError(<Error>error);
+    }
   }
 }
 
@@ -14,6 +18,15 @@ export class GetFilmsStub implements GetFilms {
     return Promise.resolve([]);
   }
 }
+
+const serverError = (error: Error) => {
+  return {
+    statusCode: 500,
+    data: {
+      message: error.message,
+    },
+  };
+};
 
 const makeSut = () => {
   const getFilmsStub = new GetFilmsStub();
@@ -32,5 +45,19 @@ describe("GetFilms Controller", () => {
     await sut.handle();
 
     expect(executeSpy).toBeCalled();
+  });
+
+  test("Should return serverError if getFilms throws", async () => {
+    const { sut, getFilmsStub } = makeSut();
+    const error = new Error("getFilms throws");
+    jest.spyOn(getFilmsStub, "execute").mockRejectedValueOnce(error);
+    const result = await sut.handle();
+
+    expect(result).toMatchObject({
+      statusCode: 500,
+      data: {
+        message: "getFilms throws",
+      },
+    });
   });
 });
